b2ff343dddab70b0c10c30936a3cb514c14da7c1
FabiolaSaga <saga.fabiola@gmail.com>
2019-02-08 13:18:06 -0600

    Added function newBallImage to generate a random number and change the UIImage based on that number. Added motionEnded method to add a shaking functionality.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,15 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ballArray: Array = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber: Int = 0
 
     @IBOutlet weak var ballImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        newBallImage()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0 ... 4)
+        ballImage.image = UIImage(named: ballArray[randomBallNumber])
     }
     
 }
